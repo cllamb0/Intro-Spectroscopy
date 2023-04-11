74834b45142098d91b60b32881d69f9c8330d8f3
Chris Lamb <cllamb777@gmail.com>
2023-04-11 01:04:22 -0700

    Slight comment change

--- Plot_Example_Data.py
+++ Plot_Example_Data.py
@@ -3,7 +3,8 @@ from spectral_functions import *
 data_dir = 'Example_Data/'
 for item in os.listdir(data_dir):
     if item == '.DS_Store':
-        continue # Using the directory so I can tab-complete (lazy)
+        # .DS_Store is a file that shows up when running on OSX, this ignores that
+        continue
     spectra_dict = import_spectra(data_dir+item)
 
     plot_spectra(spectra_dict, item)
